667d9402312cc595e0fc392a5d72c402378d0d64
hiroaki1976 <38617847+hiroaki1976@users.noreply.github.com>
2018-05-06 13:06:11 +0900

    20180506

---
+++ js/main.js
@@ -0,0 +1,49 @@
+function mouseover1(){
+  document.getElementById('editor').innerText = 'Atom';
+}
+function mouseover2(){
+  document.getElementById('languages').innerText = 'HTML5,CSS3,JavaScript';
+}
+function mouseover3(){
+  document.getElementById('os').innerText = 'Windows10';
+}
+function mouseover4(){
+  document.getElementById('browser').innerText = 'Chrome';
+}
+function mouseover5(){
+  document.getElementById('mobile').innerText = 'XperiaXZ1';
+}
+function mouseover6(){
+  document.getElementById('pc').innerText = 'Surface Pro4';
+}
+function mouseover7(){
+  document.getElementById('crypto').innerText = 'Ethereum';
+}
+function mouseover8(){
+  document.getElementById('music').innerText = 'Denki Groove';
+}
+
+function mouseout1(){
+  document.getElementById('editor').innerText = 'Editor';
+}
+function mouseout2(){
+  document.getElementById('languages').innerText = 'Languages';
+}
+function mouseout3(){
+  document.getElementById('os').innerText = 'OS';
+}
+function mouseout4(){
+  document.getElementById('browser').innerText = 'Browser';
+}
+function mouseout5(){
+  document.getElementById('mobile').innerText = 'Mobile';
+}
+function mouseout6(){
+  document.getElementById('pc').innerText = 'PC';
+}
+function mouseout7(){
+  document.getElementById('crypto').innerText = 'Cryptocurrency';
+}
+function mouseout8(){
+  document.getElementById('music').innerText = 'Music';
+}
