2f1d06ab40072821977d3736c878ba68f11ea2bf
hiroaki1976 <38617847+hiroaki1976@users.noreply.github.com>
2018-10-22 17:47:16 +0900

    20181022

--- js/main.js
+++ js/main.js
@@ -1,49 +1,50 @@
-function mouseover1(){
-  document.getElementById('editor').innerText = 'Atom';
-}
-function mouseover2(){
-  document.getElementById('languages').innerText = 'HTML5,CSS3,JavaScript';
-}
-function mouseover3(){
-  document.getElementById('os').innerText = 'Windows10';
-}
-function mouseover4(){
-  document.getElementById('browser').innerText = 'Chrome';
-}
-function mouseover5(){
-  document.getElementById('mobile').innerText = 'XperiaXZ1';
-}
-function mouseover6(){
-  document.getElementById('pc').innerText = 'Surface Pro4';
-}
-function mouseover7(){
-  document.getElementById('crypto').innerText = 'Ethereum';
-}
-function mouseover8(){
-  document.getElementById('music').innerText = 'Denki Groove';
-}
+$(function() {
+  $('#editor').click(function(){
+    $('#editorDiscription').fadeIn();
+  });
+  $('#editorDiscription').click(function(){
+    $('#editorDiscription').fadeOut();
+  });
 
-function mouseout1(){
-  document.getElementById('editor').innerText = 'Editor';
-}
-function mouseout2(){
-  document.getElementById('languages').innerText = 'Languages';
-}
-function mouseout3(){
-  document.getElementById('os').innerText = 'OS';
-}
-function mouseout4(){
-  document.getElementById('browser').innerText = 'Browser';
-}
-function mouseout5(){
-  document.getElementById('mobile').innerText = 'Mobile';
-}
-function mouseout6(){
-  document.getElementById('pc').innerText = 'PC';
-}
-function mouseout7(){
-  document.getElementById('crypto').innerText = 'Cryptocurrency';
-}
-function mouseout8(){
-  document.getElementById('music').innerText = 'Music';
-}
+  $('#languages').click(function(){
+    $('#languagesDiscription').fadeIn();
+  });
+  $('#languagesDiscription').click(function(){
+    $('#languagesDiscription').fadeOut();
+  });
+
+  $('#os').click(function(){
+    $('#osDiscription').fadeIn();
+  });
+  $('#osDiscription').click(function(){
+    $('#osDiscription').fadeOut();
+  });
+
+  $('#browser').click(function(){
+    $('#browserDiscription').fadeIn();
+  });
+  $('#browserDiscription').click(function(){
+    $('#browserDiscription').fadeOut();
+  });
+
+  $('#cryptocurrency').click(function(){
+    $('#cryptocurrencyDiscription').fadeIn();
+  });
+  $('#cryptocurrencyDiscription').click(function(){
+    $('#cryptocurrencyDiscription').fadeOut();
+  });
+
+  $('#pc').click(function(){
+    $('#pcDiscription').fadeIn();
+  });
+  $('#pcDiscription').click(function(){
+    $('#pcDiscription').fadeOut();
+  });
+
+  $('#music').click(function(){
+    $('#musicDiscription').fadeIn();
+  });
+  $('#musicDiscription').click(function(){
+    $('#musicDiscription').fadeOut();
+  });
+});
